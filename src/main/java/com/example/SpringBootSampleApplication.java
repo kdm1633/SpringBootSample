ba9966277a28252722d2e81de1beea5efa50d0b1
kdm1633 <kdm1633@outlook.com>
2023-02-23 06:58:05 +0900

    Update SpringBootSampleApplication.java

--- src/main/java/com/example/SpringBootSampleApplication.java
+++ src/main/java/com/example/SpringBootSampleApplication.java
@@ -1,11 +1,7 @@
 package com.example;
 
-import org.apache.ibatis.type.MappedTypes;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.repository.UserMapper;
 
 @SpringBootApplication
 public class SpringBootSampleApplication {
