779cfd5fdd0e6abcfef1cdfcb43f2f40b0c799a7
kdm1633 <kdm1633@outlook.com>
2023-03-18 05:33:35 +0900

    Ch11 - Spring Security

--- src/main/java/com/example/controller/LogoutController.java
+++ src/main/java/com/example/controller/LogoutController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+/** This is no longer used after Ch11 - Spring Security */
 @Controller
 @Slf4j
 public class LogoutController {
